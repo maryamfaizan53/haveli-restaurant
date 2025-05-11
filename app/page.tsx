3c45a7b3649f956858676a2f879ab2364379f315
Ramisa <ramisafatima67@gmail.com>
2025-05-11 13:52:28 +0500

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import Gallery from '@/app/components/gallery/gallery';
 import Contact from '@/app/components/contact/page';
 import Navbar from './components/Navbar';
 import Menu from './components/menu/menu';
+import HomePage from './components/home/home';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HomePage/>
       <Menu/>
       <Gallery />
       <Contact />
